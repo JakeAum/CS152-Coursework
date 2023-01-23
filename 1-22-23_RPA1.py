e359913a2d8362813ef91aece4869d5d04eb8a52
JakeAum <jacobauman00@gmail.com>
2023-01-23 12:48:30 -0700

    Updated Lecture File

--- 1-22-23_RPA1.py
+++ 1-22-23_RPA1.py
@@ -1,12 +1,12 @@
-age = 42
+age = 4.33333339
 
 print(age)
+
+print(f'{age:.2f}')
+
+print(f'{age:.4f}')
 
 print(f'{age:e}')
 
 print(f'{age:d}')
-
-print(f'{age:.2f}')
-
-print(f'{age:f}')
 
--- 1-23-23_lecture.py
+++ 1-23-23_lecture.py
@@ -1,5 +1,61 @@
 # Jacob Auman
 # 1/23/23 Scratch work for lecture
 
+# Practice problem
+
+num1= int(input('Enter Side 1: '))
+num2= int(input('Enter Side 2: '))
+num3= int(input('Enter Side 3: '))
+
+avg = (num1 + num2 + num3 )/3
+print(f'The Average is {avg}')
+
+ans1 = num1 / num2
+print(f'num1 / num2 is {ans1}')
+
+ans2 = num1 % num2
+print(f'num1 % num2 is {ans2}')
+
+ans3 = num1 // num2
+print(f'num1 // num2 is {ans3}')
+
+area1 = num1 * num1
+print(f'Rectangle area of num1 is {area1}')
+
+area2 = ( num1 * num2 )/2
+print(f'Rectangle area of num1 is {area2}')
+
+area3 =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x = 10
+print(x)
+mem_address = id(x)  # Every Object Contains a unique memory address
+print(f"Your Variable's Memory Id is {mem_address}")
+
+B = 5/2     # Standard Division
+C = 5 // 2  # Floor or the lowest Integer that did divide
+D = 5 % 2  # Modulo returns remainder form floor division
+print(B, C, D)
+
 A = int(input('Enter Integer: '))
 print(f'Your Number Is {A}')
